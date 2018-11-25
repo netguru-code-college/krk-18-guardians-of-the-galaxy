22965e7865b089d4acdc46a18868ce5493a35292
monika <monika.a.zygmunt@gmail.com>
2018-11-25 11:50:00 +0100

    rspec fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before do
+    user = User.create(email: 'sample@email.com', password: 'password')
+    sign_in user
+  end
 
   describe "GET #profile" do
     it "returns http success" do
